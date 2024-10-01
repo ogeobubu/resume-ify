4eff16df99ac123b51a641897639da55931d3f84
ogeobubu <ogeobubu@gmail.com>
2024-10-01 17:49:37 +0100

    recent changes

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import { toast } from "react-toastify"
 
 export const signin = async (data) => {
   try {
-    const response = await axios.post(`http://localhost:3000/api/signin`, data)
+    const response = await axios.post(`/api/signin`, data)
     toast.success(response.data.message)
   } catch (error) {
     return toast.error(error.response?.data?.message || "An error occurred! Try again later.")
@@ -12,7 +12,7 @@ export const signin = async (data) => {
 
 export const signup = async (data) => {
   try {
-    const response = await axios.post(`http://localhost:3000/api/signup`, data)
+    const response = await axios.post(`/api/signup`, data)
     toast.success(response.data.message)
   } catch (error) {
     return toast.error(error.response?.data?.message || "An error occurred! Try again later.")
