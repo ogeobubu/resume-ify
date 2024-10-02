19a882b4c7e8e94766b8f61004876335a28ad661
ogeobubu <ogeobubu@gmail.com>
2024-10-02 10:35:02 +0100

    recent changes

--- server/server.js
+++ server/server.js
@@ -131,7 +131,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
